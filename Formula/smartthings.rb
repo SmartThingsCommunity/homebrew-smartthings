049879344b67e6e10dbc43ccaea4634ef4bab407
John Undersander <19363370+john-u@users.noreply.github.com>
2022-06-30 11:59:51 -0500

    chore: bump smartthings @smartthings/cli@1.0.0-beta.9

--- Formula/smartthings.rb
+++ Formula/smartthings.rb
@@ -1,8 +1,8 @@
 class Smartthings < Formula
   desc "Command Line Interface for the SmartThings APIs"
   homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
-  url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.0.0-beta.8/smartthings-macos.tar.gz"
-  sha256 "302dc2df934bdc6134882725e79dbca311d4e84abc2b9db30726614cd457461b"
+  url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.0.0-beta.9/smartthings-macos.tar.gz"
+  sha256 "05050457118a41e1eda150883c1d791df68e0b3f1a246a440d2ded048f986065"
   license "Apache-2.0"
 
   def install
