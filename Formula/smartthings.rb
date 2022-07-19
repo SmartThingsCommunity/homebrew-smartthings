c64bea5ee6e3fa53ec1a74280aaff0621a10692d
John Undersander <19363370+john-u@users.noreply.github.com>
2022-07-19 11:06:41 -0500

    fix: add livecheck block to resolve versions

--- Formula/smartthings.rb
+++ Formula/smartthings.rb
@@ -1,9 +1,13 @@
 class Smartthings < Formula
-  desc "Command Line Interface for the SmartThings APIs"
+  desc "Command-line Interface for the SmartThings APIs"
   homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
   url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.0.0-beta.11/smartthings-macos.tar.gz"
   sha256 "c91a5d1c51c51002f98e0342d18cef770ccc86f50532b480edfb1666d6667002"
   license "Apache-2.0"
+
+  livecheck do
+    url :url
+  end
 
   def install
     bin.install "smartthings"
