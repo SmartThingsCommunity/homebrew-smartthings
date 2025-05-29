98b8b8806c1ddd36d55a81b3ad91d3927d577ad8
Ross Stenersen <rossiam@gmail.com>
2025-05-29 10:05:08 -0500

    Update Intel binary version and SHA

--- Formula/smartthings.rb
+++ Formula/smartthings.rb
@@ -3,8 +3,8 @@ class Smartthings < Formula
   homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
 
   if Hardware::CPU.intel?
-    url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.10.4/smartthings-macos-x64.tar.gz"
-    sha256 "14f40f809076e686f00942b7dbe5f7438ad49a27cf591b981a6b72524d44f9be"
+    url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.10.5/smartthings-macos-x64.tar.gz"
+    sha256 "124e09dd28ed352916041b70593fc122752ff2a7c4dae1b444a0688f72f9322e"
   elsif Hardware::CPU.arm?
     url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.10.5/smartthings-macos-arm64.tar.gz"
     sha256 "bd594b5be090313a8ce9934ab52ca4bf7bddabedda7d1e26c15eb10f24cae480"
