c9a590b9cbe34e0d4c588c4d400fbe3d5e219ba0
Ross Stenersen <rossiam@gmail.com>
2025-10-06 10:37:55 -0500

    add Intel MacOS binary; require at least MacOS 13.5

--- Formula/smartthings-prerelease.rb
+++ Formula/smartthings-prerelease.rb
@@ -2,9 +2,13 @@ class SmartthingsPrerelease < Formula
   desc "Command-line Interface for the SmartThings APIs, prerelease version"
   homepage "https://github.com/SmartThingsCommunity/smartthings-cli/tree/yargs"
 
+  if MacOS.version < :ventura
+    odie "Requires MacOS 13.5 or newer."
+  end
+
   if Hardware::CPU.intel?
-    url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/v2.0.0-prerelease1/smartthings-macos-x64.tar.gz"
-    disable! date: "2025-10-02", because: "does not currently support Intel Macs, please continue to use the stable formula", replacement: "smartthingscommunity/smartthings/smartthings"
+    url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/v2.0.0-prerelease1/smartthings-mac-x64.tgz"
+    sha256 "5106b90b7b4122dfb2bd3ec7bb946faa3fc788e0f10dc9ff37d1fd22e0482694"
   elsif Hardware::CPU.arm?
     url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/v2.0.0-prerelease1/smartthings-mac-arm64.tgz"
     sha256 "7471e0fa91936ffe71c219067df537bde5379b5666aa9342243a96245119295a"
